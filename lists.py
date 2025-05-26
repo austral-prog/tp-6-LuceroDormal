e5a5ba89b322571060447e2ad1ff15c35ddf1e8d
LuceroDormal <ldormal@mail.austral.edu.ar>
2025-05-26 20:39:04 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -15,13 +15,13 @@ def is_empty(list_to_check):
 
 def check_lists(list_to_compare1, list_to_compare2):
 	if len(list_to_compare1) > 2 and len(list_to_compare2) > 2: 
-		return list_to_compare1[2] == list_of_lists[2]
+		return list_to_compare1[2] == list_to_compare2[2]
 	else: 
 		return False 
 
 
 def list_of_lists(list_of_lists_to_modify):
-	first = list_of_lists_to_modify;[0][:2]
+	first = list_of_lists_to_modify[0][:2]
 	second = list_of_lists_to_modify[1][1:4]
 	third = list_of_lists_to_modify[2][-2:]
 
